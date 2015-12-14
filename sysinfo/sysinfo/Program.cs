5149eca006bf61e3afcecaef32a4f6317d61a9e9
eschneit <christian.gubesch@gmail.com>
2015-12-14 14:13:12 +0100

    Update Program.cs

--- sysinfo/sysinfo/Program.cs
+++ sysinfo/sysinfo/Program.cs
@@ -8,7 +8,7 @@ namespace sysinfo
     {
         static void Main(string[] args)
         {
-            String TITLE = "System Information";
+            String TITLE = "System Information:";
             String SPACER = "";
             String OS_NAME = String.Format("Operating System: {0}", Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ProductName", "Unknown").ToString());
             String OS_BUILD = String.Format("Build Number: {0}", Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "CurrentBuild", "Unknown").ToString());
@@ -16,7 +16,7 @@ namespace sysinfo
             String OS_KERNEL_VERSION = String.Format("Kernel Version: {0}", Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "CurrentVersion", "Unknown").ToString());
             String OS_ROOT = String.Format("System Root: {0}", Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "SystemRoot", "Unknown").ToString());
 
-            String[] values = { TITLE, SPACER, OS_NAME, OS_BUILD, OS_BRANCH, OS_KERNEL_VERSION, OS_ROOT };
+            String[] values = { SPACER, TITLE, SPACER, OS_NAME, OS_BUILD, OS_BRANCH, OS_KERNEL_VERSION, OS_ROOT };
 
             foreach (String s in values)
             {
